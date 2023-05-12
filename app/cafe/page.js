a614e62c7e5a982eccd0cfeafbe9c70b881d3287
Rowoon Lee <dev.rowoon@gmail.com>
2023-05-12 18:31:37 +0900

    몽고db연결완료

--- app/cafe/page.js
+++ app/cafe/page.js
@@ -1,3 +1,8 @@
-export default function Cafe() {
+import { connectDB } from "@/util/database";
+
+export default async function Cafe() {
+  let db = (await connectDB).db("forum");
+  let result = await db.collection("post").find().toArray();
+  console.log(result);
   return <div>카페 추천 게시글 페이지</div>;
 }
