b19f88869d987fb5cf74934fc51a391e709e5993
Rowoon Lee <dev.rowoon@gmail.com>
2023-05-12 19:20:56 +0900

    사진만 넣어놨음

--- app/cafe/page.js
+++ app/cafe/page.js
@@ -1,8 +1,20 @@
+import SectionTitle from "@/components/SectionTitle";
 import { connectDB } from "@/util/database";
+import Link from "next/link";
 
 export default async function Cafe() {
   let db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
-  console.log(result);
-  return <div>카페 추천 게시글 페이지</div>;
+  return (
+    <div>
+      <SectionTitle>카페추천</SectionTitle>
+      <ul>
+        <li>
+          <Link href="/">
+            <img src="/카페추천샘플.svg" />
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
