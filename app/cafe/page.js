065f40b4140b3ea7afbf48063b35636cd9c7f619
Rowoon Lee <dev.rowoon@gmail.com>
2023-05-13 19:08:32 +0900

    여백 조정

--- app/cafe/page.js
+++ app/cafe/page.js
@@ -10,7 +10,7 @@ export default async function Cafe() {
     <div>
       <SectionTitle>카페추천</SectionTitle>
       <button
-        className="mt-[15px] flex sm:inline-flex justify-center items-center bg-teal-500 hover:bg-teal-600 active:bg-teal-700 focus-visible:ring ring-teal-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-2"
+        className="mt-[15px] ml-[10px] m-[10px] flex sm:inline-flex justify-center items-center bg-teal-500 hover:bg-teal-600 active:bg-teal-700 focus-visible:ring ring-teal-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-2"
         type="button"
       >
         글쓰기
