f5d9f604ec53088c8f3167cdb6be66bcace0e6af
Rowoon Lee <dev.rowoon@gmail.com>
2023-05-13 19:17:12 +0900

    게시글 저장하기 완료

--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -1,10 +1,18 @@
 import { connectDB } from "@/util/database";
 
 export default async function handler(req, res) {
-  console.log(req);
   if (req.method == "POST") {
-    let db = (await connectDB).db("forum");
-    let result = await db.collection("post").insertOne(req.body);
-    return res.redirect(302, "/cafe");
+    if (req.body.title == "") {
+      return req.status(500).json("제목을 작성해주세요");
+    } else if (req.body.content == "") {
+      return req.status(500).json("내용을 작성해주세요");
+    }
+    try {
+      let db = (await connectDB).db("forum");
+      let result = await db.collection("post").insertOne(req.body);
+      return res.redirect(302, "/cafe");
+    } catch (error) {
+      alert("서버오류");
+    }
   }
 }
