596bff7f7f2e1c212902bd5e5bf846d58eff4f8a
Rowoon Lee <dev.rowoon@gmail.com>
2023-05-13 15:53:57 +0900

    키값 추가함

--- app/cafe/page.js
+++ app/cafe/page.js
@@ -5,14 +5,16 @@ import { connectDB } from "@/util/database";
 export default async function Cafe() {
   let db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
-
   return (
     <div>
       <SectionTitle>카페추천</SectionTitle>
       <div className="bg-[#F9FAFF] p-[10px]">
         {result.map((list) => {
           return (
-            <div className="bg-[white] rounded-[10px] p-[20px] mb-[5px] shadow-[0_2px_4px_0px_rgb(224,224,224)]">
+            <div
+              className="bg-[white] rounded-[10px] p-[20px] mb-[5px] shadow-[0_2px_4px_0px_rgb(224,224,224)]"
+              key={list._id}
+            >
               <h4 className="text-[20px] text-[#675D50] font-[800] m-[0]">
                 {list.title}
               </h4>
