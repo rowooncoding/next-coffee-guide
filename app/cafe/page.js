df720877a52c7778226e6edc5b0a5903a2d2ec7c
Rowoon Lee <dev.rowoon@gmail.com>
2023-05-13 16:17:18 +0900

    디테일 페이지 연결하기

--- app/cafe/page.js
+++ app/cafe/page.js
@@ -1,10 +1,12 @@
 import SectionTitle from "@/app/mainsection/SectionTitle";
 import { connectDB } from "@/util/database";
+import Link from "next/link";
 // import Link from "next/link";
 
 export default async function Cafe() {
   let db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
+  console.log(result);
   return (
     <div>
       <SectionTitle>카페추천</SectionTitle>
@@ -15,9 +17,11 @@ export default async function Cafe() {
               className="bg-[white] rounded-[10px] p-[20px] mb-[5px] shadow-[0_2px_4px_0px_rgb(224,224,224)]"
               key={list._id}
             >
-              <h4 className="text-[20px] text-[#675D50] font-[800] m-[0]">
-                {list.title}
-              </h4>
+              <Link href={"/cafe/cafedetail/" + list._id.toString()}>
+                <h4 className="text-[20px] text-[#675D50] font-[800] m-[0]">
+                  {list.title}
+                </h4>
+              </Link>
               <p className="text-[gray] my-[5px] mx-[0]">1월 1일</p>
             </div>
           );
