41426a945ce0514edf9275943518767f95c18740
Rowoon Lee <dev.rowoon@gmail.com>
2023-05-13 15:11:00 +0900

    게시판 스타일 설정

--- app/cafe/page.js
+++ app/cafe/page.js
@@ -8,13 +8,14 @@ export default async function Cafe() {
   return (
     <div>
       <SectionTitle>카페추천</SectionTitle>
-      <ul>
-        <li>
-          <Link href="/">
-            <img src="/카페추천샘플.svg" />
-          </Link>
-        </li>
-      </ul>
+      <div className="bg-[#F9FAFF] p-[10px]">
+        <div className="bg-[white] rounded-[10px] p-[20px] mb-[5px] shadow-[0_2px_4px_0px_rgb(224,224,224)]">
+          <h4 className="text-[20px] text-[#675D50] font-[800] m-[0]">
+            글제목
+          </h4>
+          <p className="text-[gray] my-[5px] mx-[0]">1월 1일</p>
+        </div>
+      </div>
     </div>
   );
 }
