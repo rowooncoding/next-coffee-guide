464d4432e61661d3c0eb1547dcb90f554053aff2
Rowoon Lee <dev.rowoon@gmail.com>
2023-05-13 19:09:36 +0900

    글쓰기 페이지로 링크 연결

--- app/cafe/page.js
+++ app/cafe/page.js
@@ -9,12 +9,14 @@ export default async function Cafe() {
   return (
     <div>
       <SectionTitle>카페추천</SectionTitle>
-      <button
-        className="mt-[15px] ml-[10px] m-[10px] flex sm:inline-flex justify-center items-center bg-teal-500 hover:bg-teal-600 active:bg-teal-700 focus-visible:ring ring-teal-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-2"
-        type="button"
-      >
-        글쓰기
-      </button>
+      <Link href="/write">
+        <button
+          className="mt-[15px] ml-[10px] m-[10px] flex sm:inline-flex justify-center items-center bg-teal-500 hover:bg-teal-600 active:bg-teal-700 focus-visible:ring ring-teal-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-2"
+          type="button"
+        >
+          글쓰기
+        </button>
+      </Link>
       <div className="bg-[#F9FAFF] p-[10px]">
         {result.map((list) => {
           return (
