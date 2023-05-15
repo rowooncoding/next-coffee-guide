74b19fbbb8d079986f5d1bb0d625fc597637afda
Rowoon Lee <dev.rowoon@gmail.com>
2023-05-15 19:31:05 +0900

    삭제 효과를 위해서 클라이언트 컴포넌트로 코드 옮김

--- app/cafe/page.js
+++ app/cafe/page.js
@@ -1,6 +1,7 @@
 import SectionTitle from "@/app/mainsection/SectionTitle";
 import { connectDB } from "@/util/database";
 import Link from "next/link";
+import ListItem from "./ListItem";
 
 export default async function Cafe() {
   let db = (await connectDB).db("forum");
@@ -17,31 +18,7 @@ export default async function Cafe() {
         </button>
       </Link>
       <div className="bg-[#F9FAFF] p-[10px]">
-        {result.map((list) => {
-          return (
-            <div
-              className="bg-[white] rounded-[10px] p-[20px] mb-[5px] shadow-[0_2px_4px_0px_rgb(224,224,224)] relative"
-              key={list._id}
-            >
-              <Link href={"/cafe/cafedetail/" + list._id.toString()}>
-                <h4 className="text-[20px] text-[#675D50] font-[800] m-[0]">
-                  {list.title}
-                </h4>
-              </Link>
-              <p className="text-[gray] my-[5px] mx-[0]">1월 1일</p>
-              <Link href={"/cafe/cafeedit/" + list._id.toString()}>
-                <button className="absolute top-2 right-12" type="button">
-                  수정
-                </button>
-              </Link>
-              <Link href={"/cafe/cafeedit/" + list._id.toString()}>
-                <button className="absolute top-2 right-3" type="button">
-                  삭제
-                </button>
-              </Link>
-            </div>
-          );
-        })}
+        <ListItem result={result} />
       </div>
     </div>
   );
