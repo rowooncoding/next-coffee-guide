2eb9da9428c1dcc4761b51552d90d5c2a47ebef1
Rowoon Lee <dev.rowoon@gmail.com>
2023-05-18 17:04:29 +0900

    원두추천 전반적인 작업 완료, 수정 기능 오류 해결중

--- app/bean/page.js
+++ app/bean/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import SectionTitle from "../mainsection/SectionTitle";
-import ListItem from "../cafe/ListItem";
 import { connectDB } from "@/util/database";
+import ListItemb from "./ListItemb";
 
 export default async function Bean() {
   let db = (await connectDB).db("forum");
@@ -9,7 +9,7 @@ export default async function Bean() {
   return (
     <div>
       <SectionTitle>원두추천</SectionTitle>
-      <Link href="/write">
+      <Link href="/writebean">
         <button
           className="mt-[15px] ml-[10px] m-[10px] flex sm:inline-flex justify-center items-center bg-teal-500 hover:bg-teal-600 active:bg-teal-700 focus-visible:ring ring-teal-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-2"
           type="button"
@@ -18,7 +18,7 @@ export default async function Bean() {
         </button>
       </Link>
       <div className="bg-[#F9FAFF] p-[10px]">
-        <ListItem result={result} />
+        <ListItemb result={result} />
       </div>
     </div>
   );
