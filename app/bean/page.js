1dafe1e5087dd0beb397a0243c68af7edbf45f53
Rowoon Lee <dev.rowoon@gmail.com>
2023-05-18 16:35:53 +0900

    데이터베이스에 있는 게시글 출력

--- app/bean/page.js
+++ app/bean/page.js
@@ -1,3 +1,25 @@
-export default function Bean() {
-  return <div>원두추천 게시판</div>;
+import Link from "next/link";
+import SectionTitle from "../mainsection/SectionTitle";
+import ListItem from "../cafe/ListItem";
+import { connectDB } from "@/util/database";
+
+export default async function Bean() {
+  let db = (await connectDB).db("forum");
+  let result = await db.collection("post2").find().toArray();
+  return (
+    <div>
+      <SectionTitle>원두추천</SectionTitle>
+      <Link href="/write">
+        <button
+          className="mt-[15px] ml-[10px] m-[10px] flex sm:inline-flex justify-center items-center bg-teal-500 hover:bg-teal-600 active:bg-teal-700 focus-visible:ring ring-teal-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-2"
+          type="button"
+        >
+          글쓰기
+        </button>
+      </Link>
+      <div className="bg-[#F9FAFF] p-[10px]">
+        <ListItem result={result} />
+      </div>
+    </div>
+  );
 }
