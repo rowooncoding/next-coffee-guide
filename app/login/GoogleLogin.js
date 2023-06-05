d7f4d9b8d4dbe689aff25277a3e18fb77b0164b4
Rowoon Lee <dev.rowoon@gmail.com>
2023-06-05 19:54:36 +0900

    구글 로그인 수정

--- app/login/GoogleLogin.js
+++ app/login/GoogleLogin.js
@@ -1,18 +1,8 @@
 "use client";
 
 import { signIn } from "next-auth/react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export default function GoogleLogin() {
-  // const session = useSession();
-  // const router = useRouter();
-  // useEffect(() => {
-  //   if (session) {
-  //     router.push("/");
-  //   }
-  // }, []);
   return (
     <button
       onClick={() => {
