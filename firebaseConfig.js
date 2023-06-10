71a0a7a4a42295bc53b505e9c43c7810338cf73f
Rowoon Lee <dev.rowoon@gmail.com>
2023-06-10 17:29:14 +0900

    firebase config 이니셜라이징

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,5 +16,11 @@ const firebaseConfig = {
   appId: "1:279505906773:web:6b6bdc8c813e07980ff8dc",
 };
 
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const storage = firebase.storage();
+export const firestore = firebase.firestore();
