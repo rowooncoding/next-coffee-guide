fbec2c4385ed6e345766788674775067d568ba07
Rowoon Lee <dev.rowoon@gmail.com>
2023-06-10 17:27:24 +0900

    firebase 설치

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCb66zlnNzF4cDhgeV5gZXE_GttmHkkMnM",
+  authDomain: "next-coffee-guide-ec085.firebaseapp.com",
+  projectId: "next-coffee-guide-ec085",
+  storageBucket: "next-coffee-guide-ec085.appspot.com",
+  messagingSenderId: "279505906773",
+  appId: "1:279505906773:web:6b6bdc8c813e07980ff8dc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
